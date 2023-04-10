0a8cdfba46b144618a0b5b7d29e938b7e111692d
DavidWolff218 <dcwolff218@gmail.com>
2023-04-10 12:07:18 -0500

    cleaned up and shortened code

--- index.js
+++ index.js
@@ -1,23 +1,53 @@
+// const distanceFromHqInBlocks = (location) => {
+//     const hq = 42
+//     if(location > 42){
+//         return location - 42
+//     } else if(location < 42){
+//         return 42 - location
+//     }
+// }
+
+// const distanceFromHqInFeet = (location) => {
+//     const totalFeet = distanceFromHqInBlocks(location) * 264
+//     return totalFeet
+// }
+
+// const distanceTravelledInFeet = (start, destination) => {
+//     if (start < destination) {
+//       return (destination - start) * 264;
+//     } else {
+//       return (start - destination) * 264;
+//     }
+//   }
+
+// const calculatesFarePrice = (start, destination) => {
+//     const distance = distanceTravelledInFeet(start, destination)
+
+//     if(distance < 400){
+//         return 0
+//     } else if(distance > 400 && distance < 2000){
+//         return .02 * (distance - 400)
+//     } else if(distance > 2000 && distance < 2500){
+//         return 25
+//     } else if(distance > 2500){
+//         return "cannot travel that far"
+//     }
+// }
+
+
+
 const distanceFromHqInBlocks = (location) => {
     const hq = 42
-    if(location > 42){
-        return location - 42
-    } else if(location < 42){
-        return 42 - location
-    }
+    const total = location > 42 ? location - 42 : 42 - location
+    return total
 }
 
 const distanceFromHqInFeet = (location) => {
-    const totalFeet = distanceFromHqInBlocks(location) * 264
-    return totalFeet
+    return distanceFromHqInBlocks(location) * 264
 }
 
 const distanceTravelledInFeet = (start, destination) => {
-    if (start < destination) {
-      return (destination - start) * 264;
-    } else {
-      return (start - destination) * 264;
-    }
+    return start < destination ? (destination - start) * 264 : (start - destination) * 264
   }
 
 const calculatesFarePrice = (start, destination) => {
@@ -29,7 +59,7 @@ const calculatesFarePrice = (start, destination) => {
         return .02 * (distance - 400)
     } else if(distance > 2000 && distance < 2500){
         return 25
-    } else if(distance > 2500){
+    } else {
         return "cannot travel that far"
     }
 }
