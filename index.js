b583e979736862e5e12466cff7e2e49a7a7d70f1
DavidWolff218 <dcwolff218@gmail.com>
2023-04-12 15:39:21 -0500

    video complete code

--- index.js
+++ index.js
@@ -1,44 +1,7 @@
-// const distanceFromHqInBlocks = (location) => {
-//     const hq = 42
-//     if(location > 42){
-//         return location - 42
-//     } else if(location < 42){
-//         return 42 - location
-//     }
-// }
-
-// const distanceFromHqInFeet = (location) => {
-//     const totalFeet = distanceFromHqInBlocks(location) * 264
-//     return totalFeet
-// }
-
-// const distanceTravelledInFeet = (start, destination) => {
-//     if (start < destination) {
-//       return (destination - start) * 264;
-//     } else {
-//       return (start - destination) * 264;
-//     }
-//   }
-
-// const calculatesFarePrice = (start, destination) => {
-//     const distance = distanceTravelledInFeet(start, destination)
-
-//     if(distance < 400){
-//         return 0
-//     } else if(distance > 400 && distance < 2000){
-//         return .02 * (distance - 400)
-//     } else if(distance > 2000 && distance < 2500){
-//         return 25
-//     } else if(distance > 2500){
-//         return "cannot travel that far"
-//     }
-// }
-
-
 
 const distanceFromHqInBlocks = (location) => {
     const hq = 42
-    const total = location > 42 ? location - 42 : 42 - location
+    const total = location > hq ? location - hq : hq - location
     return total
 }
 
@@ -47,19 +10,20 @@ const distanceFromHqInFeet = (location) => {
 }
 
 const distanceTravelledInFeet = (start, destination) => {
-    return start < destination ? (destination - start) * 264 : (start - destination) * 264
-  }
+   return start < destination ? (destination - start) * 264 : (start - destination) * 264
+}
 
 const calculatesFarePrice = (start, destination) => {
     const distance = distanceTravelledInFeet(start, destination)
-
     if(distance < 400){
         return 0
     } else if(distance > 400 && distance < 2000){
-        return .02 * (distance - 400)
+        return (distance - 400) * .02
     } else if(distance > 2000 && distance < 2500){
         return 25
     } else {
-        return "cannot travel that far"
+        return 'cannot travel that far'
     }
 }
+
+
